e60b0348c1a01cb85f8f0d15b73d1165c3f0a281
unknown <abdullah.320409@gmail.com>
2022-06-22 16:58:50 +0500

    first commit

---
+++ Js/app.js
@@ -0,0 +1,118 @@
+function toggleServicesDropDown() {
+    document.getElementById("services-drop-down").classList.toggle("show-services");
+    document.getElementById("services-drop-down").classList.toggle("show-services-dropdown");
+    document.getElementById("service-dropdown-arrow").classList.toggle("toggle-dropdown-arrow");
+}
+
+function toggleLoginDropDown() {
+    document.getElementById("login-drop-down").classList.toggle("show-login");
+    document.getElementById("login-drop-down").classList.toggle("show-login-dropdown");
+}
+
+function expandCard1() {
+    document.getElementById("chevron-up-1").classList.toggle("toggle-chevron-1");
+    document.getElementById("card-expand-content-1").classList.toggle("expand-card-1");
+}
+
+function expandCard2() {
+    document.getElementById("chevron-up-2").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-2").classList.toggle("expand-card");
+}
+
+function expandCard3() {
+    document.getElementById("chevron-up-3").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-3").classList.toggle("expand-card");
+}
+
+function expandCard4() {
+    document.getElementById("chevron-up-4").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-4").classList.toggle("expand-card");
+}
+
+function expandCard5() {
+    document.getElementById("chevron-up-5").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-5").classList.toggle("expand-card");
+}
+
+function expandCard6() {
+    document.getElementById("chevron-up-6").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-6").classList.toggle("expand-card");
+}
+
+function expandCard7() {
+    document.getElementById("chevron-up-7").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-7").classList.toggle("expand-card");
+}
+
+function expandCard8() {
+    document.getElementById("chevron-up-8").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-8").classList.toggle("expand-card");
+}
+
+function expandCard9() {
+    document.getElementById("chevron-up-9").classList.toggle("toggle-chevron");
+    document.getElementById("card-expand-content-9").classList.toggle("expand-card");
+}
+
+function expandContent1() {
+    document.getElementById("plus-1").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-1").classList.toggle("expand");
+}
+
+function expandContent2() {
+    document.getElementById("plus-2").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-2").classList.toggle("expand");
+}
+
+function expandContent3() {
+    document.getElementById("plus-3").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-3").classList.toggle("expand");
+}
+
+function expandContent4() {
+    document.getElementById("plus-4").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-4").classList.toggle("expand");
+}
+
+function expandContent5() {
+    document.getElementById("plus-5").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-5").classList.toggle("expand");
+}
+
+function expandContent6() {
+    document.getElementById("plus-6").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-6").classList.toggle("expand");
+}
+
+function expandContent7() {
+    document.getElementById("plus-7").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-7").classList.toggle("expand");
+}
+
+function expandContent8() {
+    document.getElementById("plus-8").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-8").classList.toggle("expand");
+}
+
+function expandContent9() {
+    document.getElementById("plus-9").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-9").classList.toggle("expand");
+}
+
+function expandContent10() {
+    document.getElementById("plus-10").classList.toggle("toggle-accordion-arrow");
+    document.getElementById("expand-content-10").classList.toggle("expand");
+}
+
+function seeMore() {
+    document.getElementById("see-more").classList.toggle("see");
+    document.getElementById("expand-accordion-1").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-2").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-3").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-4").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-5").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-6").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-7").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-8").classList.toggle("accordion-expand-all");
+    document.getElementById("expand-accordion-9").classList.toggle("accordion-expand-all");
+}
